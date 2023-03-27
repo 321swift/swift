c28384369a9b42a6973969a9c13e2f0639db480b
1jack80 <work.davidmainoo@gmail.com>
2023-03-27 15:14:44 +0000

    add function to send broadcast message

---
+++ sendBroadcast.py
@@ -0,0 +1,17 @@
+import socket
+
+def send_broadcast_message(message, port=5000):
+    # Set the broadcast address for the current network
+    broadcast_address = "<your_broadcast_address>"
+
+    # Create a UDP socket
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    # Enable broadcasting on the socket
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+
+    # Send the message to the broadcast address
+    sock.sendto(message.encode(), (broadcast_address, port))
+
+    # Close the socket
+    sock.close()
