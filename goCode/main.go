ec569ba3178e78f4a37ec0d5b77078aa5ef884f6
1jack80 <work.davidmainoo@gmail.com>
2023-03-29 20:46:22 +0000

    can now send and receive messages

--- goCode/main.go
+++ goCode/main.go
@@ -11,20 +11,26 @@ import (
 var wg sync.WaitGroup
 
 func main() {
-	wg.Add(1)
+	// wg.Add(1)
 
-	// listen for message on the broadcast address
-	message := make(chan []byte)
-	listener(message)
-	fmt.Print(string(<-message))
+	// // listen for message on the broadcast address
+	// message := make(chan []byte)
+	// go listener(message)
+	// fmt.Print(string(<-message))
 
-	// send message on the broadcast address with port 5050
-	for i := 0; i < 10; i++ {
-		c.SendMessage(u.GetBroadcastAddress(), 5050, "hi there I am David")
-		fmt.Printf("sent %d \n", i)
-		time.Sleep(time.Second * 3)
-	}
-	close(message)
+	// // send message on the broadcast address with port 5050
+	// for i := 0; i < 10; i++ {
+	// 	c.SendMessage(u.GetBroadcastAddress(), 5050, "hi there I am David")
+	// 	fmt.Printf("sent %d \n", i)
+	// 	time.Sleep(time.Second * 3)
+	// }
+	// close(message)
+	// c.ListenForBroadcast()
+
+	go sender()
+	message, _ := c.ListenForBroadcast(5050)
+	fmt.Println(string(message))
+
 }
 
 func listener(message chan []byte) {
@@ -32,4 +38,15 @@ func listener(message chan []byte) {
 
 	bytes, _ := c.ListenForBroadcast(5050)
 	message <- bytes
+}
+
+func sender() {
+
+	// // send message on the broadcast address with port 5050
+	for i := 0; i < 10; i++ {
+		c.SendMessage(u.GetBroadcastAddress(), 5050, "hi there I am David")
+		fmt.Printf("sent %d \n", i)
+		time.Sleep(time.Second * 3)
+	}
+
 }
--- goCode/utils/getBroadcastAddress.go
+++ goCode/utils/getBroadcastAddress.go
@@ -1,17 +1,12 @@
 package utils
 
-import "fmt"
-
 func GetBroadcastAddress() string {
-	fmt.Println("Begin obtaining broadcast address")
 	interfaces, err := GetUpnRunninginterfaces()
 	if err != nil {
 		panic("error while getting up and runnign interfaces")
 	}
-	fmt.Println("Extracting ipv4 Address")
 	addr, _ := ExtractIPV4Address(interfaces[0])
 
-	fmt.Println("Calculating broadcast")
 	addr, _ = CalcBroadcastAddress(addr)
 	return addr
 }
