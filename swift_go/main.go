6ff578e0e11e2bb9b8a1f2984fa4e7727133a664
1jack80 <work.davidmainoo@gmail.com>
2023-03-29 18:25:55 +0000

    get all up and running interfaces except loopback and virtualbox

--- swift_go/main.go
+++ swift_go/main.go
@@ -6,31 +6,6 @@ import (
 	"strings"
 )
 
-// GetBroadcastAddress returns the broadcast address of the current network
-func GetBroadcastAddress() (net.IP, error) {
-	// Get the default route
-	route, err := net.InterfaceAddrs()
-	if err != nil {
-		return nil, err
-	}
-
-	// Loop through the interfaces and find the default route
-	for _, r := range route {
-		if ipnet, ok := r.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
-			if ipnet.IP.To4() != nil {
-				// Calculate the broadcast address using the subnet mask
-				broadcastIP := make(net.IP, len(ipnet.IP))
-				copy(broadcastIP, ipnet.IP)
-				for i := range broadcastIP {
-					broadcastIP[i] |= ^ipnet.Mask[i]
-				}
-				return broadcastIP, nil
-			}
-		}
-	}
-	return nil, fmt.Errorf("no default route found")
-}
-
 // GetLocalIPAddress returns the IP address of the current network
 func GetLocalIPAddress() (net.IP, net.IPMask, error) {
 	// Get the default route
@@ -50,9 +25,26 @@ func GetLocalIPAddress() (net.IP, net.IPMask, error) {
 	return nil, nil, fmt.Errorf("no default route found")
 }
 
-func PrintAllIp() {
-	route, err := net.InterfaceAddrs()
-	fmt.Print(route, err)
+// GetBroadcastAddress calculates the broadcast address for a given IP address and subnet
+func GetBroadcastAddress(ipAddress string) (string, error) {
+	// Parse the IP address and subnet
+	ip, ipNet, err := net.ParseCIDR(ipAddress)
+	if err != nil {
+		return "", err
+	}
+
+	// Get the network size in bits
+	ones, bits := ipNet.Mask.Size()
+
+	// Calculate the broadcast address
+	mask := net.CIDRMask(ones, bits)
+	network := ip.Mask(mask)
+	broadcast := make(net.IP, len(network))
+	for i := range network {
+		broadcast[i] = network[i] | ^mask[i]
+	}
+
+	return broadcast.String(), nil
 }
 
 func main() {
@@ -67,7 +59,11 @@ func main() {
 			strings.Contains(flags.String(), "up") &&
 			!strings.Contains(interf.Name, "VirtualBox") &&
 			!strings.Contains(interf.Name, "Loopback") {
-			print(interf)
+			addresses, err := interf.Addrs()
+			if err != nil {
+
+			}
+			print(GetBroadcastAddress(addresses[1].String()))
 		}
 	}
 }
