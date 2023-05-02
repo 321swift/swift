e95ec8dc8a623d9b9c1c3b13ef22e2476783b437
1jack80 <work.davidmainoo@gmail.com>
2023-05-02 03:28:35 +0000

    Begin final app flow

--- main.go
+++ main.go
@@ -1,21 +1,14 @@
 package main
 
 import (
-	"fmt"
-	"swift2/backend/client"
-	"swift2/global"
-	"swift2/ui2"
-	"time"
+	"log"
+	"os"
+	"swift/node"
 )
 
 func main() {
-	// create new listener
-	client := client.NewClient()
-	time.AfterFunc(time.Second*8, func() {
-		client.Connect(fmt.Sprintf(":%d", global.BackendServerPort))
-	})
-
-	// start server
-	server := ui2.NewUiServer()
-	server.Start()
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+	node := node.NewNode(infoLog, errorLog)
+	node.Start()
 }
