c46d23480036a5441d030191a43a49d61a2805ed
1jack80 <work.davidmainoo@gmail.com>
2023-06-24 02:50:52 +0000

    refactore build script

--- build.sh
+++ build.sh
@@ -1,22 +1,28 @@
 # windows
 # 32-bit
-GOOS=windows GOARCH=386 go  build -x -o bin/windows/swift-386.exe main.go
+GOOS=windows GOARCH=386 go  build  -o dist/windows/swift-386.exe main.go
+7z a -tzip ./dist/windows/swift-386-windows.zip ./dist/windows/swift-386.exe ./ui
 
 # 64-bit
-GOOS=windows GOARCH=amd64 go  build -x -o bin/windows/swift-amd64.exe main.go
-
+GOOS=windows GOARCH=amd64 go  build  -o dist/windows/swift-amd64.exe main.go
+7z a -tzip ./dist/windows/swift-amd64-windows.zip ./dist/windows/swift-amd64.exe ./ui
 
 # macos
 # 64-bit
-GOOS=darwin GOARCH=amd64 go  build -x -o bin/macos/swift-amd64-darwin main.go
+GOOS=darwin GOARCH=amd64 go  build  -o dist/macos/swift-amd64-darwin main.go
+7z a -tzip ./dist/macos/swift-amd64-darwin.zip ./dist/macos/swift-amd64-darwin ./ui
 
-# 32-bit
-# GOOS=darwin GOARCH=386 go  build -x -o bin/macos/swift-386-darwin main.go
+# # linux
+# # 64-bit
+GOOS=linux GOARCH=amd64 go  build  -o dist/linux/swift-amd64-linux main.go
+7z a -tzip ./dist/linux/swift-amd64-linux.zip ./dist/linux/swift-amd64-linux ./ui
 
+# # 32-bit
+GOOS=linux GOARCH=386 go  build  -o dist/linux/swift-386-linux main.go
+7z a -tzip ./dist/linux/swift-386-linux.zip ./dist/linux/swift-386-linux ./ui
 
-# linux
-# 64-bit
-GOOS=linux GOARCH=amd64 go  build -x -o bin/linux/swift-amd64-linux main.go
-
-# 32-bit
-GOOS=linux GOARCH=386 go  build -x -o bin/linux/swift-386-linux main.go
+rm ./dist/linux/swift-386-linux
+rm ./dist/linux/swift-amd64-linux
+rm ./dist/macos/swift-amd64-darwin
+rm ./dist/windows/swift-386.exe
+rm ./dist/windows/swift-amd64.exe
