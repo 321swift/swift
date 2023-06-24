d42e11f111be32e650f99be2d8ac389b7684b324
1jack80 <work.davidmainoo@gmail.com>
2023-06-24 01:25:18 +0000

    remove build for macos 386 arch; err, not valid pairing and modify build processes to verbose

--- build.sh
+++ build.sh
@@ -1,22 +1,22 @@
 # windows
 # 32-bit
-GOOS=windows GOARCH=386 go build -o bin/windows/swift-386.exe main.go
+GOOS=windows GOARCH=386 go -v build -o bin/windows/swift-386.exe main.go
 
 # 64-bit
-GOOS=windows GOARCH=amd64 go build -o bin/windows/swift-amd64.exe main.go
+GOOS=windows GOARCH=amd64 go -v build -o bin/windows/swift-amd64.exe main.go
 
 
 # macos
 # 64-bit
-GOOS=darwin GOARCH=amd64 go build -o bin/macos/swift-amd64-darwin main.go
+GOOS=darwin GOARCH=amd64 go -v build -o bin/macos/swift-amd64-darwin main.go
 
 # 32-bit
-GOOS=darwin GOARCH=386 go build -o bin/macos/swift-386-darwin main.go
+# GOOS=darwin GOARCH=386 go -v build -o bin/macos/swift-386-darwin main.go
 
 
 # linux
 # 64-bit
-GOOS=linux GOARCH=amd64 go build -o bin/linux/swift-amd64-linux main.go
+GOOS=linux GOARCH=amd64 go -v build -o bin/linux/swift-amd64-linux main.go
 
 # 32-bit
-GOOS=linux GOARCH=386 go build -o bin/linux/swift-386-linux main.go
+GOOS=linux GOARCH=386 go -v build -o bin/linux/swift-386-linux main.go
